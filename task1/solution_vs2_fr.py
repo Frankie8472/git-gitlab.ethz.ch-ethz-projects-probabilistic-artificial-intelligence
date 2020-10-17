cf65d64097fed0c98adb6a400528fd6623446cf6
Franz Knobel <knobelf@student.ethz.ch>
2020-10-17 17:08:34 +0200

    innerproduct, nystroem, gpr

--- task1/solution_vs2_fr.py
+++ task1/solution_vs2_fr.py
@@ -1,6 +1,8 @@
 import numpy as np
-from sklearn.gaussian_process.kernels import RBF, ConstantKernel
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel, DotProduct
 from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.kernel_approximation import RBFSampler, Nystroem
 
 ## Constant for Cost function
 THRESHOLD = 0.5
@@ -57,19 +59,36 @@ It uses predictions to compare to the ground truth using the cost_function above
 
 class Model():
     def __init__(self):
-        kernel = ConstantKernel(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
-        self.model = GaussianProcessRegressor(kernel=kernel, alpha=0.1**2, n_restarts_optimizer=10)
+        print("==> Initializing")
+        kernel1 = ConstantKernel(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
+        kernel2 = DotProduct(sigma_0=0.0)
+        self.nystroem = Nystroem(kernel=kernel1, random_state=1, n_components=20)
+        self.model = GaussianProcessRegressor(kernel=kernel2, alpha=0.1**2, n_restarts_optimizer=0)
         return
 
     def predict(self, test_x):
+        print("==> Predicting")
+        print("====> Nystroem")
+        test_x = self.nystroem.transform(test_x)
+        print("====> MatMul")
+        #test_x = test_x @ test_x.T
+        print("====> GPR")
         return self.model.predict(test_x)
 
     def fit_model(self, train_x, train_y):
+        print("==> Training")
+        print("====> Nystroem")
+        train_x = self.nystroem.fit_transform(train_x, train_y)
+        print("====> MatMul")
+        #train_x = train_x @ train_x.T
+        print("====> GPR")
         self.model.fit(train_x, train_y)
         return
 
 
 def main():
+    test_size = None
+    print("==> Loading")
     train_x_name = "train_x.csv"
     train_y_name = "train_y.csv"
 
@@ -79,9 +98,8 @@ def main():
     # load the test dateset
     test_x_name = "test_x.csv"
     test_x = np.loadtxt(test_x_name, delimiter=',')
-
     M = Model()
-    M.fit_model(train_x, train_y)
+    M.fit_model(train_x[:test_size], train_y[:test_size])
     prediction = M.predict(test_x)
 
     print(prediction)
