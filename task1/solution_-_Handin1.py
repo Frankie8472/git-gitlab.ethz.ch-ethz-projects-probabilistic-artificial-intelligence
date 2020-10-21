1122456c54ba5ee0c66c25caa076b04064054c8b
Frankie <knobelf@student.ethz.ch>
2020-10-21 21:30:22 +0200

    optimizing, statistically stabilizing and reordering+documenting code

--- task1/solution_-_Handin1.py
+++ task1/solution_-_Handin1.py
@@ -1,19 +1,16 @@
 import numpy as np
-#import pathos.multiprocessing as mp
-from sklearn.ensemble import BaggingRegressor, AdaBoostRegressor
-from sklearn.gaussian_process.kernels import RBF, ConstantKernel, DotProduct, WhiteKernel
+from sklearn.ensemble import BaggingRegressor
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel, WhiteKernel
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.pipeline import Pipeline
-from sklearn.kernel_approximation import RBFSampler, Nystroem
 
-## Constant for Cost function
+# Constants for the following functions
 THRESHOLD = 0.5
 W1 = 1
 W2 = 20
 W3 = 100
 W4 = 0.04
 SEED = 42
-CORES = 2#mp.cpu_count()
+CORES = 4
 
 
 def cost_function(true, predicted):
@@ -23,58 +20,71 @@ def cost_function(true, predicted):
 
         return: float
     """
-    cost = (true - predicted)**2
+    cost = (true - predicted) ** 2
 
     # true above threshold (case 1)
     mask = true > THRESHOLD
-    mask_w1 = np.logical_and(predicted>=true,mask)
-    mask_w2 = np.logical_and(np.logical_and(predicted<true,predicted >=THRESHOLD),mask)
-    mask_w3 = np.logical_and(predicted<THRESHOLD,mask)
+    mask_w1 = np.logical_and(predicted >= true, mask)
+    mask_w2 = np.logical_and(np.logical_and(predicted < true, predicted >= THRESHOLD), mask)
+    mask_w3 = np.logical_and(predicted < THRESHOLD, mask)
 
-    cost[mask_w1] = cost[mask_w1]*W1
-    cost[mask_w2] = cost[mask_w2]*W2
-    cost[mask_w3] = cost[mask_w3]*W3
+    cost[mask_w1] = cost[mask_w1] * W1
+    cost[mask_w2] = cost[mask_w2] * W2
+    cost[mask_w3] = cost[mask_w3] * W3
 
     # true value below threshold (case 2)
     mask = true <= THRESHOLD
-    mask_w1 = np.logical_and(predicted>true,mask)
-    mask_w2 = np.logical_and(predicted<=true,mask)
+    mask_w1 = np.logical_and(predicted > true, mask)
+    mask_w2 = np.logical_and(predicted <= true, mask)
 
-    cost[mask_w1] = cost[mask_w1]*W1
-    cost[mask_w2] = cost[mask_w2]*W2
+    cost[mask_w1] = cost[mask_w1] * W1
+    cost[mask_w2] = cost[mask_w2] * W2
 
-    reward = W4*np.logical_and(predicted < THRESHOLD,true<THRESHOLD)
+    reward = W4 * np.logical_and(predicted < THRESHOLD, true < THRESHOLD)
     if reward is None:
         reward = 0
     return np.mean(cost) - np.mean(reward)
+
 
 """
 Fill in the methods of the Model. Please do not change the given methods for the checker script to work.
 You can add new methods, and make changes. The checker script performs:
 
-
     M = Model()
-    M.fit_model(train_x,train_y)
+    M.fit_model(train_x, train_y)
     prediction = M.predict(test_x)
 
 It uses predictions to compare to the ground truth using the cost_function above.
 """
 
 
-class Model():
+class Model:
+    """Model for Task1 Gaussian process regression (GPR).
+
+        This model tackles four challenges:
+            1. Model selection: With a combination of a Linear-, RBF- and
+               Whitenoise-Kernel the GP should be modelled faithfully.
+            2. Large scale learning: To overcome the computationally intensivenenss,
+               we used the Bagging method from sklearn (without bootstrapping).
+            3. Asymmetric costs: To ensure that there are no false positives,
+               we add the std to the output mean to only get false negatives.
+
+        """
+
     def __init__(self):
+        """Inits Model with a kernel, a classifier (gpr) and a bagging method."""
         print("==> Initializing")
-        kernel = RBF(length_scale=1) * ConstantKernel(1) + WhiteKernel(0.1)
+        kernel = ConstantKernel() * RBF() + WhiteKernel()
         gpr = GaussianProcessRegressor(
             kernel=kernel,
-            alpha=1e-3,
-            n_restarts_optimizer=2,
+            alpha=1e-4,
+            n_restarts_optimizer=3,
             normalize_y=True,
             random_state=SEED
         )
         self.model = BaggingRegressor(
             base_estimator=gpr,
-            n_estimators=16,
+            n_estimators=20,
             max_samples=1000,
             max_features=1.0,
             bootstrap=False,
@@ -83,34 +93,27 @@ class Model():
             n_jobs=CORES,
             random_state=SEED
         )
-
-        # self.model2 = AdaBoostRegressor(
-        #     base_estimator=gpr,
-        #     n_estimators=30,
-        #     learning_rate=1.0,
-        #     loss='linear'
-        # )
-
         return
 
     def predict(self, test_x):
+        """Performs a upper boundary prediction on a given test set (test_x)."""
         print("==> Predicting")
-        print("====> Bagging with Subsampling")
         y_preds = list()
         for model in self.model.estimators_:
             y_pred_mean, y_pred_std = model.predict(test_x, return_std=True)
-            y_pred = y_pred_mean + 1.2 * y_pred_std
+            y_pred = y_pred_mean + 1.195 * y_pred_std
             y_preds.append(y_pred)
         return np.asarray(y_preds).mean(axis=0)
 
     def fit_model(self, train_x, train_y):
+        """Fits the model on the given train set (train_x) for the given output (train_y)."""
         print("==> Training")
-        print("====> Bagging with Subsampling")
         self.model.fit(train_x, train_y)
         return
 
 
 def main():
+    """Used for local testing only."""
     test_size = None
     print("==> Loading")
     train_x_name = "train_x.csv"
